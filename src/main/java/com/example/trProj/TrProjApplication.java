58e889b3383cf98687b4f36dea2d46e6d89521be
Kwon042 <miri9612@gmail.com>
2025-02-27 15:22:57 +0900

    스프링이니셜라이저 파일 생성

---
+++ src/main/java/com/example/trProj/TrProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.trProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trProj/TrProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
